3a4c025a1227730b0fcbafe806a5e151486f9cf4
zxw <103567041@qq.com>
2017-12-05 13:23:02 +0800

    openssl_encrypt 代替原有的加密和解密方法

--- src/MyWeChat/WeChat/CryptLib/PrpCrypt.php
+++ src/MyWeChat/WeChat/CryptLib/PrpCrypt.php
@@ -28,18 +28,12 @@ class PrpCrypt
             //获得16位随机字符串，填充到明文之前
             $random = $this->getRandomStr();
             $text = $random . pack("N", strlen($text)) . $text . $appid;
-            // 网络字节序
-            $size = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
-            $module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
-            $iv = substr($this->key, 0, 16);
             //使用自定义的填充方式对明文进行补位填充
             $pkc_encoder = new PKCS7Encoder();
             $text = $pkc_encoder->encode($text);
-            mcrypt_generic_init($module, $this->key, $iv);
-            //加密
-            $encrypted = mcrypt_generic($module, $text);
-            mcrypt_generic_deinit($module);
-            mcrypt_module_close($module);
+
+            $iv = substr($this->key, 0, 16);
+            $encrypted = openssl_encrypt($text, 'aes-256-cbc', $this->key, OPENSSL_RAW_DATA, $iv);
 
             //print(base64_encode($encrypted));
             //使用BASE64对加密后的字符串进行编码
@@ -56,18 +50,11 @@ class PrpCrypt
      */
     public function decrypt($encrypted, $appid)
     {
-
         try {
             //使用BASE64对需要解密的字符串进行解码
-            $ciphertext_dec = base64_decode($encrypted);
-            $module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
             $iv = substr($this->key, 0, 16);
-            mcrypt_generic_init($module, $this->key, $iv);
-
-            //解密
-            $decrypted = mdecrypt_generic($module, $ciphertext_dec);
-            mcrypt_generic_deinit($module);
-            mcrypt_module_close($module);
+            $ciphertext_dec = base64_decode($encrypted);
+            $decrypted = openssl_decrypt($ciphertext_dec, 'aes-256-cbc', $this->key, OPENSSL_RAW_DATA, $iv);
         } catch (Exception $e) {
             return ErrorCode::$DecryptAESError;
         }
